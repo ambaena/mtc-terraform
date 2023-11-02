5390b9e88481b3c64e0d7b1d55dcff8fc3ac7d4c
Alfonso Manuel Baena González <alfonsobg.94@gmail.com>
2023-11-02 10:42:17 +0100

    :sparkles: feat: add 03-modular-deployments

--- 03-modular-deployments/main.tf
+++ 03-modular-deployments/main.tf
@@ -1,35 +1,35 @@
-resource "null_resource" "dockervol" {
-  provisioner "local-exec" {
-    command = "mkdir noderedvol/ || true && sudo chown -R 1000:1000 noderedvol/"
+locals {
+  deployment = {
+    nodered = {
+      container_count = length(var.ext_port["nodered"][terraform.workspace])
+      image = var.image["nodered"][terraform.workspace]
+      int = 1880
+      ext = var.ext_port["nodered"][terraform.workspace]
+      container_path = "/data"
+    }
+    influxdb = {
+      container_count = length(var.ext_port["influxdb"][terraform.workspace])
+      image = var.image["influxdb"][terraform.workspace]
+      int = 8086
+      ext = var.ext_port["influxdb"][terraform.workspace]
+      container_path = "/var/lib/influxdb"
+    }
   }
 }
 
 module "image" {
   source   = "./image"
-  image_in = var.image[terraform.workspace]
+  for_each = local.deployment
+  image_in = each.value.image
 }
 
-resource "docker_image" "nodered_image" {
-  name = var.image[terraform.workspace]
-}
-
-resource "random_string" "random" {
-  count   = local.container_count
-  length  = 4
-  special = false
-  upper   = false
-}
-
-resource "docker_container" "nodered_container" {
-  count = local.container_count
-  name  = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
-  image = module.image.image_out
-  ports {
-    internal = var.int_port
-    external = var.ext_port[terraform.workspace][count.index]
-  }
-  volumes {
-    container_path = "/home/data"
-    host_path      = "${path.cwd}/noderedvol/"
-  }
+module "container" {
+  source            = "./container"
+  count_in = each.value.container_count
+  for_each = local.deployment
+  name_in           = each.key
+  image_in          = module.image[each.key].image_out
+  int_port_in       = each.value.int
+  ext_port_in       = each.value.ext
+  container_path_in = each.value.container_path
 }
--- 03-modular-deployments/outputs.tf
+++ 03-modular-deployments/outputs.tf
@@ -1,9 +1,4 @@
-output "ip-address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.network_data[0].ip_address, i.ports[0]["external"]])]
-  description = "The IP address of the container"
-}
-
-output "container-name" {
-  value       = docker_container.nodered_container[*].name
-  description = "The name of the container"
+output "application_access" {
+    value = [for x in module.container[*]: x]
+    description = "The name and socket for each application."
 }
