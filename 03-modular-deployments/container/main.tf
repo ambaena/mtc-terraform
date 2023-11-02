2a919774b6c616d04bc92b3735a632c35059407a
Alfonso Manuel Baena González <alfonsobg.94@gmail.com>
2023-11-02 16:17:37 +0100

    :sparkles: feat: add image nested module and fmt

--- 03-modular-deployments/container/main.tf
+++ 03-modular-deployments/container/main.tf
@@ -13,11 +13,11 @@ resource "docker_container" "app_container" {
     internal = var.int_port_in
     external = var.ext_port_in[count.index]
   }
-  dynamic volumes {
+  dynamic "volumes" {
     for_each = var.volumes_in
-    content {      
+    content {
       container_path = volumes.value["container_path_each"]
-      volume_name    = docker_volume.container_volume[volumes.key].name
+      volume_name    = module.volume[count.index].volume_output[volumes.key]
     }
   }
   provisioner "local-exec" {
@@ -31,21 +31,9 @@ resource "docker_container" "app_container" {
   }
 }
 
-resource "docker_volume" "container_volume" {
-  count = length(var.volumes_in)
-  name  = "${var.name_in}-${count.index}-volume"
-  lifecycle {
-    prevent_destroy = false
-  }
-  provisioner "local-exec" {
-    when       = destroy
-    command    = "mkdir ${path.cwd}/../backup/"
-    on_failure = continue
-  }
-  # Docker in MacOs runs in VM, so volume are not accesibles directly from host
-  # provisioner "local-exec" {
-  #   when = destroy
-  #   command = "sudo tar -czvf ${path.cwd}/../backup/${self.name}.tar.gz ${self.mountpoint}/"
-  #   on_failure = fail
-  # }
+module "volume" {
+  source       = "./volume"
+  count        = var.count_in
+  volume_count = length(var.volumes_in)
+  volume_name  = "${var.name_in}-${terraform.workspace}-${random_string.random[count.index].result}-volume"
 }
